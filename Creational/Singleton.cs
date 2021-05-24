7466e6d866a922a00bd422e48e50833d656138b7
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-05-24 17:18:19 +0200

    Hide lazy instance behind property

--- Creational/Singleton.cs
+++ Creational/Singleton.cs
@@ -1,7 +1,7 @@
 void Main()
 {
-    var properties1 = Properties.Instance.Value;
-    var properties2 = Properties.Instance.Value;
+    var properties1 = Properties.Instance;
+    var properties2 = Properties.Instance;
 
     $"Using {nameof(properties1)}, the application name is: {properties1.ApplicationName}".Dump();
     $"Using {nameof(properties2)}, the application name is: {properties2.ApplicationName}".Dump();
@@ -11,13 +11,14 @@ void Main()
 // Singleton class, allows for at most one unique instance of an object to exist
 public class Properties
 {
-    public string ApplicationName { get; private set; } = string.Empty;
-
     // Unique instance that can be accessed by the outside
     // Using the lazy initialization, we only create the element once: when requested
     // for the first time
-    public static Lazy<Properties> Instance
-        = new Lazy<Properties>(() => new Properties());
+    private static Lazy<Properties> _lazyInstance = new Lazy<Properties>(() => new Properties());
+
+    public string ApplicationName { get; private set; } = string.Empty;
+
+    public static Properties Instance { get => _lazyInstance.Value; }
 
     // The constructor must be private to ensure that no other instance can be
     // created from the outside
